3127a99353d83f4a99ae3d66cdd34b2994d8f824
Tim Bradley <fennasnogothrim@gmail.com>
2019-02-04 01:37:10 -0700

    Created infrastructure code package

---
+++ packages/rank-my-reps-infrastructure/README.md
@@ -0,0 +1,11 @@
+# Rank My Reps - Infrastructure
+
+> TODO: description
+
+## Usage
+
+```
+const rankMyRepsInfrastructure = require('rank-my-reps-infrastructure');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/rank-my-reps-infrastructure/src/index.ts
@@ -0,0 +1,69 @@
+import AWS from "aws-sdk";
+import { config } from "dotenv";
+import fs from "fs";
+import {
+  CreateStackInput,
+  UpdateStackInput
+} from "aws-sdk/clients/cloudformation";
+
+config();
+
+const cloudFormation = new AWS.CloudFormation({
+  apiVersion: "2010-05-15",
+  region: process.env.AWS_REGION || "us-east-1"
+});
+
+export default async function createInfrastructure() {
+  console.log(`Fetching stack info...`);
+  const stackConf: CreateStackInput | UpdateStackInput = {
+    StackName: process.env.STACK_NAME || "RankMyReps",
+    Tags: [
+      {
+        Key: "project",
+        Value: "rank-my-reps"
+      }
+    ],
+    TemplateBody: fs
+      .readFileSync(`${__dirname}/../templates/cloudfront-rank-my-reps.json`)
+      .toString()
+  };
+
+  const stacks =
+    (await cloudFormation.listStacks().promise()).StackSummaries || [];
+
+  let previousStack = stacks.find(
+    s =>
+      s.StackName === stackConf.StackName && s.StackStatus !== "DELETE_COMPLETE"
+  );
+  console.log(JSON.stringify(previousStack, undefined, 4));
+  if (previousStack && previousStack.StackStatus === "ROLLBACK_COMPLETE") {
+    console.log(`Previous stack rolled back, deleting it first...`);
+    await cloudFormation
+      .deleteStack({ StackName: stackConf.StackName })
+      .promise();
+    await cloudFormation
+      .waitFor("stackDeleteComplete", { StackName: stackConf.StackName })
+      .promise();
+    console.log(`Old stack deleted.`);
+    previousStack = undefined;
+  }
+
+  if (previousStack) {
+    console.log(`Updating Rank My Reps infrastructure...`);
+    console.log(
+      `Updated Stack ${
+        (await cloudFormation.updateStack(stackConf).promise()).StackId
+      }`
+    );
+  } else {
+    console.log(`Creating Rank My Reps infrastructure...`);
+    console.log(
+      `Created Stack ${
+        (await cloudFormation.createStack(stackConf).promise()).StackId
+      }`
+    );
+  }
+  console.log(`Infrastructure generation complete.`);
+}
+
+createInfrastructure();
