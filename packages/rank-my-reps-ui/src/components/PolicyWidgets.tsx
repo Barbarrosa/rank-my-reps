2129afa73fdde95de40289ebc3963e69af72779b
Tim Bradley <fennasnogothrim@gmail.com>
2019-02-05 23:07:50 -0700

    Fix scorecard state init bug; remove unused import; add prod build script

--- packages/rank-my-reps-ui/src/components/PolicyWidgets.tsx
+++ packages/rank-my-reps-ui/src/components/PolicyWidgets.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import * as React from "react";
 
 const loadPolicyLib = (() => {
--- packages/rank-my-reps-ui/src/state/ScoreState.ts
+++ packages/rank-my-reps-ui/src/state/ScoreState.ts
@@ -1,34 +1,55 @@
 import { useEffect, useState } from "react";
-import { Score, ScoreCard, subscribeToScoreCard } from "../fn/scorecard";
+import {
+  getScoreCard,
+  Score,
+  ScoreCard,
+  subscribeToScoreCard,
+  updateScoreCard
+} from "../fn/scorecard";
 
-const convertScorecard = (card: ScoreCard): Array<{vote: string} & Score> & { original?: ScoreCard } => {
-    const converted: Array<{vote: string} & Score> & { original?: ScoreCard } = Object.entries(card)
-        .map(([vote, score]) => ({vote, ...score}));
-    converted.original = card;
-    return converted;
+const convertScorecard = (
+  card: ScoreCard
+): Array<{ vote: string } & Score> & { original?: ScoreCard } => {
+  const converted: Array<{ vote: string } & Score> & {
+    original?: ScoreCard;
+  } = Object.entries(card).map(([vote, score]) => ({ vote, ...score }));
+  converted.original = card;
+  return converted;
 };
 
-export default function getScoreState(userId: string): { scores: Array<{vote:string} & Score> & { original?: ScoreCard }, loading: boolean } {
-    const [scores,setScores] = useState([] as Array<{vote:string} & Score> & { original?: ScoreCard });
-    const [loading,setLoading] = useState(true);
-    useEffect(() => {
-        let generator: AsyncIterableIterator<ScoreCard>;
-        (async () => {
-            generator = subscribeToScoreCard(userId);
-            setLoading(true);
-            setScores(convertScorecard((await generator.next()).value));
-            setLoading(false);
+export default function getScoreState(
+  userId: string
+): {
+  scores: Array<{ vote: string } & Score> & { original?: ScoreCard };
+  loading: boolean;
+} {
+  const [scores, setScores] = useState([] as Array<{ vote: string } & Score> & {
+    original?: ScoreCard;
+  });
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    let generator: AsyncIterableIterator<ScoreCard>;
+    (async () => {
+      // Ensure scorecard exists
+      await updateScoreCard(userId, await getScoreCard(userId));
 
-            for await(const scorecard of generator) {
-                setScores(convertScorecard(scorecard));
-            }
-        })();
-        return () => {
-            if(generator && generator.return) { generator.return(); }
-        };
-    },[userId]);
-    return {
-        loading,
-        scores,
+      generator = subscribeToScoreCard(userId);
+      setLoading(true);
+      setScores(convertScorecard((await generator.next()).value));
+      setLoading(false);
+
+      for await (const scorecard of generator) {
+        setScores(convertScorecard(scorecard));
+      }
+    })();
+    return () => {
+      if (generator && generator.return) {
+        generator.return();
+      }
     };
+  }, [userId]);
+  return {
+    loading,
+    scores
+  };
 }
